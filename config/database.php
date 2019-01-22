c888734ddcf8dbf432a95632ab29ad6567eeb01e
Narayan Sharma <narayansharma@ucreate.co.in>
2019-01-22 13:29:33 +0530

    testing

--- config/database.php
+++ config/database.php
@@ -83,7 +83,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'test'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', 'postgres'),
+            'password' => env('DB_PASSWORD', 'ucreate'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
